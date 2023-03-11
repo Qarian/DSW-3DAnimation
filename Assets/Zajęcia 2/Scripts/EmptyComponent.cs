3ffc18406821bb2d68300263489b8bea62d2829a
Sir Quarian <sir.quarian@theknightsofunity.com>
2023-03-11 19:29:48 +0100

    Initial commit

---
+++ Assets/Zajęcia 2/Scripts/EmptyComponent.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// using są na górze pliku i dają dostęp do zaawansowanych typów - w przyszłości będziemy dodawać inne
+
+// public jest modyfikatorem dostępu - mówi że to co jest po nim może być używane przez inne klasy.
+//        Innym modyfikatorem jest private, który na to nie pozwala. Jak nie ma podanego modyfikatora to domyślnie jest private
+// class jest słowem kluczowym które dajemy przed nazwą klasy
+// EmptyComponent to nazwa tej klasy, musi to być dokładnie ta sama nazwa co nawa pliku, inaczej Unity nie pozwoli dodać tego skryptu jako komponentu
+// MonoBehaviour po dwukropku sprawia że nasza klasa może być używana jako komponent, mamy dostep do Start, Update i innych
+public class EmptyComponent : MonoBehaviour
+{
+    // Komentarze można robić na 2 sposoby:
+    // 2 ukośniki komentują wszystko co jest na prawo od nich 
+    
+    /* Ukośnik z gwiazdą zaczynają komentarz na więcej niż 1 linię.
+     Koniec komentarza oznacza się gwiazdą i ukośnikiem (odwrotna kolejność)*/
+    
+    // W C# znaki białe (spacja, tabulacja, koniec lini) służą do odzielenia słów od siebie, nie ma znaczenia ile ich jest pomiędzy
+}
---
+++ Assets/Zajęcia 2/Scripts/Mover.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Przesuwa obiekt 
+public class Mover : MonoBehaviour
+{
+    // Domyślnie skrypt będzie przesuwał obiekt, do którego jest przypięty, o jedną jednostkę na sekundę w osi X (w prawo)
+    public Vector3 speed = new Vector3(1, 0, 0);
+
+    // Update is called once per frame
+    private void Update()
+    {
+        // Time.deltaTime zwraca czas trwania klatki. Dzięki temu obiekt będzie przesuwał się z taką samą prędkością
+        // nie ważne ile będzie klatek na sekundę (jak często wywoła się Update)
+        GetComponent<Transform>().position += speed * Time.deltaTime;
+        
+        // += pozwala nam zapisać poniższą linijkę w prostszy sposób
+        //GetComponent<Transform>().position = GetComponent<Transform>().position + (speed * Time.deltaTime);
+    }
+}
